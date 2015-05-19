a9532b03f77f90e19bd5228ec811baa5fa8c5c29
chenjiancan <allenjiancan@gmail.com>
2015-05-19 15:16:43 +0800

    测试带参数的 url - index

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(req):
+    return HttpResponse("Hey boy!")
+
+
+def detail(req, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def result(req, question_id):
+    return HttpResponse("You're looking at result of question %s." % question_id)
+
+def vote(req, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
--- tutorial_polls_site/settings.py
+++ tutorial_polls_site/settings.py
@@ -104,5 +104,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'templates'),
 )
